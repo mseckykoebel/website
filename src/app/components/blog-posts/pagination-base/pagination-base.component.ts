18ef481852ebcbd67fd52562a2c080d44a91f4e0
Mason Secky-Koebel <mseckykoebel@gmail.com>
2019-10-10 11:05:51 -0500

    fixed the issue with cropping on the text, and replaced the feature image

---
+++ src/app/components/blog-posts/pagination-base/pagination-base.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination-base',
+  templateUrl: './pagination-base.component.html',
+  styleUrls: ['./pagination-base.component.css']
+})
+export class PaginationBaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
