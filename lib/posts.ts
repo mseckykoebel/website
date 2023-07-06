9cd05bb6252d5396ef78bf840e2acb3014b25527
Mason Secky-Koebel <mseckykoebel@gmail.com>
2023-07-06 07:16:45 -0500

    fix: tsignore of this line, types are fine

--- lib/posts.ts
+++ lib/posts.ts
@@ -56,7 +56,7 @@ export async function getPostData(id: string) {
 
   // Use remark to convert markdown into HTML string
   const processedContent = await remark()
-    .use(html)
+    .use(html) //@ts-ignore
     .process(matterResult.content);
   const contentHtml = processedContent.toString();
 
