86b33abea595d09eda5a160ce3e848e08ea86620
Mason Secky-Koebel <mseckykoebel@gmail.com>
2023-09-02 22:08:48 -0500

    feat: adding vercel analytics;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/global.css";
 import "highlight.js/styles/github.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
