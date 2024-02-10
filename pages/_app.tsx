8593f4c91b0bec485378dc7e704bacd7548d2300
Mason Secky-Koebel <mseckykoebel@gmail.com>
2024-02-10 16:37:12 -0600

    feat: add posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { PostHogProvider } from "posthog-js/react";
+
 import "../styles/global.css";
 import "highlight.js/styles/github.css";
 
+const apiKey = "phc_7Lg7HDoMB56ztjIAWBZN3YJ5vv8JDWNjiei6Dbn7wRO";
+const options = {
+  api_host: "https://app.posthog.com",
+};
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <PostHogProvider apiKey={apiKey} options={options}>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </PostHogProvider>
   );
 };
 
